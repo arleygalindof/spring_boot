427f3e00d988a5a6092ec4ab561d125a4a230cc3
JCPosso <juancamiloposso01@gmail.com>
2025-09-26 09:13:26 -0500

    Refactor project structure and update test configurations
    - Updated file paths in the Maven compiler plugin input and created files to reflect new directory structure.
    - Added Surefire reports for SpringBootCatalogApplicationTests, indicating successful test execution.
    - Created application-test.properties and application.properties for H2 in-memory database configuration during tests.
    - Added logging configuration for SQL and Spring JDBC during tests.
    - Compiled SpringBootCatalogApplicationTests class file.

--- src/test/java/com/acn/springboot/catalog/app/SpringBootCatalogApplicationTests.java
+++ src/test/java/com/acn/springboot/catalog/app/SpringBootCatalogApplicationTests.java
@@ -2,8 +2,10 @@ package com.acn.springboot.catalog.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringBootCatalogApplicationTests {
 
 	@Test
